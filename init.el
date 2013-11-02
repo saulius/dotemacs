dbf87cc791f1c3666161c27d7bf30a0771a21532
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-02 21:15:49 +0200

    No menus, no splash screens

--- init.el
+++ init.el
@@ -1,5 +1,13 @@
 (add-to-list 'load-path "~/.emacs.d/")
 
+;; Turn off graphical interface early on, console person
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; Turn off splash screen
+(setq inhibit-startup-screen t)
+
 ;; Initialize Cask, the package manager
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
