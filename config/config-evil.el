310abd3b006f35a641aca657c6c676edc4a8f208
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-21 21:14:29 +0200

    Preserve cursor color with evil

--- config/config-evil.el
+++ config/config-evil.el
@@ -2,6 +2,15 @@
 (require 'evil-leader)
 (require 'surround)
 
+;; Syntactic sugar for eval-after-load dance.
+;; https://github.com/purcell/emacs.d/blob/aa789c9745b13612c4fea6e638d81d8ebbfecdf8/init-utils.el#L1-L5
+
+(defmacro after-load (feature &rest body)
+  "After FEATURE is loaded, evaluate BODY."
+  (declare (indent defun))
+  `(eval-after-load ,feature
+     '(progn ,@body)))
+
 (evil-mode 1)
 (global-surround-mode 1)
 (global-evil-leader-mode 1)
@@ -12,3 +21,11 @@
 ;; keyboard shortcuts
 (evil-leader/set-key
   "j" 'projectile-find-file)
+
+(after-load 'evil
+  (setq
+   ;; this stops evil from overwriting the cursor color
+   evil-default-cursor t
+   evil-default-state 'normal
+   )
+)
