dd9b2dfd6b836b2f58c0fc60c7e44768ac12aace
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-21 21:07:50 +0200

    Add evil-leader and evil-surround

--- Cask
+++ Cask
@@ -3,7 +3,9 @@
 
 (depends-on "cask")
 (depends-on "evil")
+(depends-on "evil-leader")
 (depends-on "grizzl")
 (depends-on "hemisu-theme")
 (depends-on "sublime-themes")
+(depends-on "surround")
 (depends-on "projectile")
--- config/config-evil.el
+++ config/config-evil.el
@@ -1,3 +1,11 @@
 (require 'evil)
+(require 'evil-leader)
+(require 'surround)
 
 (evil-mode 1)
+(global-surround-mode 1)
+(global-evil-leader-mode 1)
+
+;; keyboard shortcuts
+(evil-leader/set-key
+  "j" 'projectile-find-file)
