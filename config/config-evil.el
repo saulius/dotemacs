d93245846ff4ee95f2c746e15838aa6d05829711
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-22 21:39:48 +0200

    Disable toolbar, scrollbars

--- config/config-evil.el
+++ config/config-evil.el
@@ -3,15 +3,6 @@
 (require 'surround)
 
 ;; TODO move to better place
-;; Syntactic sugar for eval-after-load dance.
-;; https://github.com/purcell/emacs.d/blob/aa789c9745b13612c4fea6e638d81d8ebbfecdf8/init-utils.el#L1-L5
-(defmacro after-load (feature &rest body)
-  "After FEATURE is loaded, evaluate BODY."
-  (declare (indent defun))
-  `(eval-after-load ,feature
-     '(progn ,@body)))
-
-;; TODO move to better place
 (defun my-switch-to-other-buffer ()
   "Switch to other buffer"
   (interactive)
--- config/config-general.el
+++ config/config-general.el
@@ -1,3 +1,15 @@
+;; TODO move to better place
+;; Syntactic sugar for eval-after-load dance.
+;; https://github.com/purcell/emacs.d/blob/aa789c9745b13612c4fea6e638d81d8ebbfecdf8/init-utils.el#L1-L5
+(defmacro after-load (feature &rest body)
+  "After FEATURE is loaded, evaluate BODY."
+  (declare (indent defun))
+  `(eval-after-load ,feature
+     '(progn ,@body)))
+
+(require 'whitespace)
+(after-load "whitespace" '(setq whitespace-style (delq 'spaces whitespace-style)))
+
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
 ; (require 'powerline)
@@ -10,3 +22,11 @@
 
 (add-to-list 'default-frame-alist '(background-color . "#FFFFFF"))
 (add-to-list 'default-frame-alist '(foreground-color . "#000000"))
+
+;; Disable toolbar
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+;; Disable scroll bar
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
