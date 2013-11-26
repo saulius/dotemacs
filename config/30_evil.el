086f7b6ac070211ac6322d8df348379083050925
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-26 17:46:04 +0200

    Rename current file&buffer with <leader>n

--- config/30_evil.el
+++ config/30_evil.el
@@ -21,6 +21,24 @@
   (save-buffer)
   )
 
+;; from http://tuxicity.se/emacs/elisp/2010/03/26/rename-file-and-buffer-in-emacs.html
+(defun rename-this-buffer-and-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond ((get-buffer new-name)
+               (error "A buffer named '%s' already exists!" new-name))
+              (t
+               (rename-file filename new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil)
+               (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
+
 (evil-mode 1)
 (global-surround-mode 1)
 (global-evil-leader-mode 1)
@@ -40,6 +58,7 @@
   "cr" 'comment-or-uncomment-region
   "b" 'projectile-switch-to-buffer
   "f" 'ido-find-file
+  "n" 'rename-this-buffer-and-file
   "k" 'kill-this-buffer
   "j" 'projectile-find-file
   "T" 'eshell
