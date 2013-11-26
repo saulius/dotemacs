7f65a85de5deb8c82b8c98e1fe1f001e6973ef4c
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-26 20:20:19 +0200

    Extract util functions

---
+++ config/10_utils.el
@@ -0,0 +1,31 @@
+;; switch to previous buffer
+(defun my-switch-to-other-buffer ()
+  "Switch to other buffer"
+  (interactive)
+  (switch-to-buffer (other-buffer)))
+
+;; save file trimming all whitespace
+(defun save-buffer-no-whitespace ()
+  "Writes buffer with trimmed whitespace"
+  (interactive)
+  (delete-trailing-whitespace)
+  (save-buffer)
+  )
+
+;; from http://tuxicity.se/emacs/elisp/2010/03/26/rename-file-and-buffer-in-emacs.html
+(defun rename-this-buffer-and-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond ((get-buffer new-name)
+               (error "A buffer named '%s' already exists!" new-name))
+              (t
+               (rename-file filename new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil)
+               (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
--- config/30_evil.el
+++ config/30_evil.el
@@ -8,37 +8,6 @@
 (require 'evil-nerd-commenter)
 (require 'surround)
 
-;; TODO move to better place
-(defun my-switch-to-other-buffer ()
-  "Switch to other buffer"
-  (interactive)
-  (switch-to-buffer (other-buffer)))
-
-(defun save-buffer-no-whitespace ()
-  "Writes buffer with trimmed whitespace"
-  (interactive)
-  (delete-trailing-whitespace)
-  (save-buffer)
-  )
-
-;; from http://tuxicity.se/emacs/elisp/2010/03/26/rename-file-and-buffer-in-emacs.html
-(defun rename-this-buffer-and-file ()
-  "Renames current buffer and file it is visiting."
-  (interactive)
-  (let ((name (buffer-name))
-        (filename (buffer-file-name)))
-    (if (not (and filename (file-exists-p filename)))
-        (error "Buffer '%s' is not visiting a file!" name)
-      (let ((new-name (read-file-name "New name: " filename)))
-        (cond ((get-buffer new-name)
-               (error "A buffer named '%s' already exists!" new-name))
-              (t
-               (rename-file filename new-name 1)
-               (rename-buffer new-name)
-               (set-visited-file-name new-name)
-               (set-buffer-modified-p nil)
-               (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
-
 (evil-mode 1)
 (global-surround-mode 1)
 (global-evil-leader-mode 1)
