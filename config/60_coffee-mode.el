0b6e77baf39828e849c5eed1eb793efb2ea0197a
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-29 23:06:02 +0200

    Indent with 2 spaces in coffeescript mode

--- config/60_coffee-mode.el
+++ config/60_coffee-mode.el
@@ -1,3 +1,10 @@
 (el-get 'sync '(coffee-mode))
 
 (require 'coffee-mode)
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
