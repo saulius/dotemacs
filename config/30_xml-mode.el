b7d267eba94d7bf637713d02e5a32c592b05e98b
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-30 22:36:29 +0200

    Treat .atom files as xml

--- config/30_xml-mode.el
+++ config/30_xml-mode.el
@@ -5,8 +5,9 @@
 ;; from prelude  https://github.com/bbatsov/prelude/blob/02b3f27e65ae6e213c8046d3196eacb24630600f/modules/prelude-xml.el
 (push '("<\\?xml" . nxml-mode) magic-mode-alist)
 
-;; pom files should be treated as xml files
+;; following files should be treated as xml files
 (add-to-list 'auto-mode-alist '("\\.pom$" . nxml-mode))
+(add-to-list 'auto-mode-alist '("\\.atom$" . nxml-mode))
 
 (setq nxml-child-indent 4)
 (setq nxml-attribute-indent 4)
