54e274b6accdab5c1455f564dd9d308a0c7aab42
Saulius Grigaliunas <saulius@ninja.lt>
2013-11-30 11:40:29 +0200

    Add XML mode

---
+++ config/30_xml-mode.el
@@ -0,0 +1,15 @@
+(el-get 'sync '(nxml-mode))
+
+(require 'nxml-mode)
+
+;; from prelude  https://github.com/bbatsov/prelude/blob/02b3f27e65ae6e213c8046d3196eacb24630600f/modules/prelude-xml.el
+(push '("<\\?xml" . nxml-mode) magic-mode-alist)
+
+;; pom files should be treated as xml files
+(add-to-list 'auto-mode-alist '("\\.pom$" . nxml-mode))
+
+(setq nxml-child-indent 4)
+(setq nxml-attribute-indent 4)
+(setq nxml-auto-insert-xml-declaration-flag nil)
+(setq nxml-bind-meta-tab-to-complete-flag t)
+(setq nxml-slash-auto-complete-flag t)
