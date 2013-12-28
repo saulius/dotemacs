d1b91898f901f907f7b677386f3e1d4b8fdb6e45
Saulius Grigaliunas <saulius@ninja.lt>
2013-12-28 10:42:23 +0200

    Open json files in js2-mode

--- config/60_javascript-mode.el
+++ config/60_javascript-mode.el
@@ -1,4 +1,5 @@
 (el-get 'sync '(js2-mode))
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.js.erb\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js.erb$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
