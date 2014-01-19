a178148f9c20b796ab20b0d35c8a39ff20e868ec
Saulius Grigaliunas <saulius@ninja.lt>
2014-01-19 12:53:57 +0200

    Do not hide dired details in Emacs 24.4+

--- config/30_dired.el
+++ config/30_dired.el
@@ -1,5 +1,9 @@
 (el-get 'sync '(dired+))
 
+; do not hide dired details by default
+(setq diredp-hide-details-initially-flag nil)
+(setq diredp-hide-details-propagate-flag nil)
+
 (add-hook 'dired-load-hook
            (lambda ()
              ;; Bind dired-x-find-file.
