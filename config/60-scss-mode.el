1a5242b1e424f273087a6b6f28f613733a147eea
Saulius Grigaliunas <saulius@ninja.lt>
2014-01-22 09:09:58 +0200

    Use scss mode for css files, enable rainbow mode

--- config/60-scss-mode.el
+++ config/60-scss-mode.el
@@ -5,3 +5,7 @@
 (setq scss-compile-at-save nil) ;; do not compile scss on save
 (setq css-indent-offset 2)
 (setq css-indent-level 2)
+
+(add-to-list 'auto-mode-alist '("\\.css\\'" . scss-mode))
+
+(add-hook 'css-mode-hook 'rainbow-mode)
