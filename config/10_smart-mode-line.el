0346013effd341768837ca366e758b4b1261e034
Saulius Grigaliunas <saulius@ninja.lt>
2014-02-08 17:47:47 +0200

    Properly set theme for smart-mode-line

--- config/10_smart-mode-line.el
+++ config/10_smart-mode-line.el
@@ -1,9 +1,9 @@
 (el-get 'sync '(smart-mode-line))
 
 (require 'smart-mode-line)
-(setq sml/theme 'light)
 (setq sml/shorten-directory t)
 (setq sml/shorten-modes t)
 (setq sml/name-width 40)
 (setq sml/mode-width 'full)
 (sml/setup)
+(sml/apply-theme 'light)
