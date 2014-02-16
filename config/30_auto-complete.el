f5210a29e51878d9efeb512ed7a3e430faf3593c
Saulius Grigaliunas <saulius@ninja.lt>
2014-02-16 15:58:50 +0200

    Turn on auto-complete in prog mode, tweak settings

--- config/30_auto-complete.el
+++ config/30_auto-complete.el
@@ -1,4 +1,13 @@
 (el-get 'sync '(auto-complete))
 
 (require 'auto-complete)
-(require 'popup)
+(require 'auto-complete-config)
+(ac-config-default)
+(setq ac-auto-show-menu t) ;; always show menu
+(setq ac-quick-help-delay 1)
+(setq ac-quick-help-height 60)
+(setq ac-show-menu-immediately-on-auto-complete t)
+(setq ac-auto-start 2)
+(setq ac-candidate-menu-min 0)
+
+(add-hook 'prog-mode-hook 'auto-complete-mode)
