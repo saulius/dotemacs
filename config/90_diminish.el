82a72c38484d19411cf763936f8d9ab1a8468d81
Saulius Grigaliunas <saulius@ninja.lt>
2014-03-08 19:38:01 +0200

    Bring back diminish to keep it tidy

---
+++ config/90_diminish.el
@@ -0,0 +1,10 @@
+(el-get 'sync '(diminish))
+
+(require 'diminish)
+
+(diminish 'rainbow-mode)
+(diminish 'projectile-mode)
+(diminish 'smartparens-mode)
+(diminish 'auto-complete-mode)
+(diminish 'whitespace-mode)
+(diminish 'undo-tree-mode)
