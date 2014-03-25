b5216ad2a622b34b8402a876f4155e760291d642
Saulius Grigaliunas <saulius@ninja.lt>
2014-03-25 09:16:54 +0200

    Projectile treats any directory as project

--- config/30_projectile.el
+++ config/30_projectile.el
@@ -8,5 +8,7 @@
 (setq projectile-enable-caching t)
 (setq projectile-project-ignored-files '("*.class" "*.o" "*.so" "*.elc" "*.jar" "*.png" "*.jpg" "*.jpeg" "*.gif"))
 (setq projectile-globally-ignored-directories (append '("target") projectile-globally-ignored-directories))
+(setq projectile-require-project-root nil) ;; any directory can be a project
+(setq projectile-show-paths-function 'projectile-hashify-with-relative-paths)
 
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
