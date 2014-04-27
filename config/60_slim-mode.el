7b63d55411bcd54478026eaa1ac337bbb0d8b133
Saulius Grigaliunas <saulius@ninja.lt>
2014-04-27 11:05:31 +0300

    Correctly indent in slim-mode

--- config/60_slim-mode.el
+++ config/60_slim-mode.el
@@ -1,3 +1,12 @@
 (el-get 'sync '(slim-mode))
 
 (require 'slim-mode)
+
+(defun slim-custom ()
+  "slim-mode-hook"
+  (local-set-key "\t" 'insert-two-spaces)
+  (set (make-local-variable 'tab-width) 2)
+  (setq evil-shift-width tab-width))
+
+(add-hook 'slim-mode-hook
+  '(lambda() (slim-custom)))
