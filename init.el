c1c94bf325734661b80e9cf04b363baffec6c45f
Saulius Grigaliunas <saulius@ninja.lt>
2014-07-12 15:50:46 +0300

    No startup splash screen

--- init.el
+++ init.el
@@ -2,6 +2,9 @@
 (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
   (when (fboundp mode) (funcall mode -1)))
 
+;; hide startup splash screen
+(setq inhibit-startup-screen t)
+
 ;;debug
 (setq debug-on-error t)
 
