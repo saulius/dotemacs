3e119faa253c8efdecedda06800e75ce2685c7d3
Saulius Grigaliunas <saulius@ninja.lt>
2014-11-08 19:33:40 +0200

    Use req-package instead of bundle
    More options, deferred loading via use-package

--- init.el
+++ init.el
@@ -5,25 +5,36 @@
 ;; hide startup splash screen
 (setq inhibit-startup-screen t)
 
-(when (version< emacs-version "24.0")
-  (error "GNU Emacs 24 is needed, current version %s" emacs-version))
+(when (version< emacs-version "24.4")
+  (error "GNU Emacs 24.4 is needed, current version %s" emacs-version))
 
 ;; Always load newest byte code
 (setq load-prefer-newer t)
 
-(add-to-list 'load-path (concat user-emacs-directory "el-get/el-get"))
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-install-skip-emacswiki-recipes el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
-;; shallow clone saves bandwidth
-(setq el-get-git-shallow-clone t)
-
-(el-get 'sync '(el-get org-mode))
+(add-to-list 'el-get-recipe-path "~/.emacs.d/elget-recipes")
+(el-get 'sync)
 
 (org-babel-load-file (expand-file-name "tao-boot.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
